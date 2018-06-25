1215c7f755dba5a7ee7aceecdc57fc941c694e5f
Conn Warwicker <conn@cmrwarwicker.com>
2018-06-25 17:08:14 +0100

    .

--- structures/gen.js
+++ structures/gen.js
@@ -110,8 +110,6 @@ class NameGenerator
                     resp += 'Here are your ' + limit + ' fantasy place names:\n\n';
                 } else if(type === 'land'){
                     resp += 'Here are your ' + limit + ' fantasy land names:\n\n';
-                } else if(type === 'book'){
-                    resp += 'Here are your ' + limit + ' book titles:\n\n';
                 } else if(type === 'book_fantasy'){
                     resp += 'Here are your ' + limit + ' fantasy book titles:\n\n';
                 } else if(type === 'book_sf'){
